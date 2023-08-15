98834f06f36b9424bc1d07c0d7fc0b0b10a150fa
dharshan.ts <dharshan.ts@ivymobility.com>
2023-08-15 20:29:33 +0530

    Added the correct origin

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-cd Week-5-with-axios-and-backend
- git pull origin master
+cd Week9
+ git pull origin3 master
  cd server
  pm2 kill
  pm2 start index.js
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "toDoKeyPair.pem" ubuntu@ec2-13-232-228-204.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
